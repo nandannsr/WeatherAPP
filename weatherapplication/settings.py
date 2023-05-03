5ab5591fd3141ea0c2f080714c8fc968336c1593
Nandan shambhu <nandannsrkumar@gmail.com>
2023-05-03 16:14:53 +0530

    Completed

--- weatherapplication/settings.py
+++ weatherapplication/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%_wlr#0mol0rmqw3xw9++)^+nnc!win15_794$01e+qv#%r)s4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
